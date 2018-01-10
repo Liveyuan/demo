b0a8e4c994ce7243e625990c4f75fd896f741a1a
Log_yuan <Log_yuan@163.com>
2018-01-10 17:48:09 +0800

    修改模型类

--- Demo1/box/models.py
+++ Demo1/box/models.py
@@ -5,9 +5,14 @@ from django.db import models
 
 # Create your models here.
 class UserInfo(models.Model):
-    uname = models.CharField(max_length=20)
-    upwd = models.CharField(max_length=40)
+    uname = models.CharField(max_length=20, verbose_name=u'用户名称')
+    upwd = models.CharField(max_length=40, verbose_name=u'用户密码')
+    gender = models.IntegerField(default=0, choices=((0, '男'),(1, '女')))
 
     def __str__(self):
         return "%s" % self.uname
-    
+
+
+
+
+

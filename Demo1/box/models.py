297899d6b2d3b6a1908cae88e0435ded7eddd278
Log_yuan <Log_yuan@163.com>
2018-01-15 19:46:21 +0800

    添加家乡

--- Demo1/box/models.py
+++ Demo1/box/models.py
@@ -8,6 +8,7 @@ class UserInfo(models.Model):
     uname = models.CharField(max_length=20, verbose_name=u'用户名称')
     upwd = models.CharField(max_length=40, verbose_name=u'用户密码')
     gender = models.IntegerField(default=0, choices=((0, '男'),(1, '女')))
+    hometown = models.CharField(default='', max_length=200, verbose_name=u'家乡')
 
     def __str__(self):
         return "%s" % self.uname

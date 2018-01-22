5340381f6a864973bdfa83b493b092480cfcd52e
Log_yuan <Log_yuan@163.com>
2018-01-22 15:52:26 +0800

    添加描述类型

--- Demo1/box/models.py
+++ Demo1/box/models.py
@@ -9,6 +9,7 @@ class UserInfo(models.Model):
     upwd = models.CharField(max_length=40, verbose_name=u'用户密码')
     gender = models.IntegerField(default=0, choices=((0, '男'),(1, '女')))
     hometown = models.CharField(default='', max_length=200, verbose_name=u'家乡')
+    desc = models.CharField(default='', max_length=200, verbose_name=u'描述')
 
     def __str__(self):
         return "%s" % self.uname
